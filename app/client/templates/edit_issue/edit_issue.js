e993e563321e4e790b5c6e4eea1bcf0d9861761f
Geno <romanelli.geno@gmail.com>
2017-11-13 16:53:44 -0500

    Finished tutorial

---
+++ app/client/templates/edit_issue/edit_issue.js
@@ -0,0 +1,13 @@
+Template.IssuesList.events({
+});
+Template.EditIssue.helpers({
+  beforeRemove: function () {
+    return function (collection, id) {
+      var doc = collection.findOne(id);
+      if (confirm('Really delete issue: "' + doc.title + '"?')) {
+        this.remove();
+        Router.go('issuesList');
+      }
+    };
+  }
+});
--- app/client/templates/issues_list/issues_list.js
+++ app/client/templates/issues_list/issues_list.js
@@ -1,23 +1,29 @@
-/*****************************************************************************/
-/* IssuesList: Event Handlers */
-/*****************************************************************************/
 Template.IssuesList.events({
 });
 
-/*****************************************************************************/
-/* IssuesList: Helpers */
-/*****************************************************************************/
 Template.IssuesList.helpers({
-});
-
-/*****************************************************************************/
-/* IssuesList: Lifecycle Hooks */
-/*****************************************************************************/
-Template.IssuesList.onCreated(function () {
-});
-
-Template.IssuesList.onRendered(function () {
-});
-
-Template.IssuesList.onDestroyed(function () {
+  issues: function () {
+    return Issues.find();
+  },
+  dueDateFormatted: function () {
+    return moment(this.dueDate).format("MMM Do YY");
+  },
+  priorityHigh: function() {
+    if (this.priority === 'High')
+      return true;
+    else
+      return false;
+  },
+  priorityMedium: function() {
+    if (this.priority === 'Medium')
+      return true;
+    else
+      return false;
+  },
+  priorityLow: function() {
+    if (this.priority === 'Low')
+      return true;
+    else
+      return false;
+  }
 });
